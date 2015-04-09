51cde3d975e5561a800fb4bd1d0e04724c24a070
zh6335901 <zh6335901@hotmail.com>
2015-04-09 21:23:15 +0800

    实现基数排序

--- Sort.Implements/8.LinearTimeSort/LinearTimeSorter.cs
+++ Sort.Implements/8.LinearTimeSort/LinearTimeSorter.cs
@@ -4,56 +4,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sort.Implements._8.LinearTimeSort
+namespace Sort.Implements
 {
     public static class LinearTimeSorter
     {
-        /// <summary>
-        /// 计数排序,输入序列的元素element必须满足: 0 <= element <= max
-        /// </summary>
-        /// <param name="array">参与排序的数组</param>
-        /// <param name="max">数组的元素可以到达的最大值</param>
-        /// <returns></returns>
-        public static int[] CountingSort(int[] array, int max, bool asc = true)
+		/// <summary>
+		/// 计数排序,输入序列的元素element必须满足: 0 <= element <= max
+		/// </summary>
+		/// <param name="array">参与排序的数组</param>
+		/// <param name="max">数组的元素可以到达的最大值</param>
+		/// <param name="asc">是否顺序排序</param>
+		/// <returns>计数排序后的数组</returns>
+		public static int[] CountingSort(int[] array, int max, bool asc = true)
         {
-            if (array == null || array.Length == 0)
-            {
-                return new int[0];
-            }
-
-            if (max < 0)
-            {
-                throw new ArgumentOutOfRangeException("max", "max必须大于等于0");
-            }
-
-            int[] countingArray = new int[max + 1];
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] < 0 || array[i] > max)
-                {
-                    throw new ArgumentOutOfRangeException("array", "数组元素必须大于等于0并小于等于max");
-                }
-
-                countingArray[array[i]] += 1;
-            }
-
-            for (int i = 1; i <= max; i++)
-            {
-                countingArray[i] += countingArray[i - 1];
-            }
-
-            int[] sortedArray = new int[array.Length];
-            //倒序是为了确保计数排序的稳定性
-            for (int i = array.Length - 1; i >= 0; i--)
-            {
-                int index = asc ? countingArray[array[i]] - 1 : sortedArray.Length - countingArray[array[i]];
-                sortedArray[index] = array[i];
-                countingArray[array[i]] -= 1;
-            }
-
-            return sortedArray;
+			return CountingSort(array, n => n, max, asc);
         }
 
+		/// <summary>
+		/// 计数排序,输入序列的元素element必须满足: 0 <= element <= max
+		/// </summary>
+		/// <param name="array">参与排序的数组</param>
+		/// <param name="getCompareValue">根据数组元素获取实际所要计数的值</param>
+		/// <param name="max">数组的元素可以到达的最大值</param>
+		/// <param name="asc">是否顺序排序</param>
+		/// <returns>计数排序后的数组</returns>
+		public static int[] CountingSort(int[] array, Func<int, int> getCompareValue, int max, bool asc = true)
+		{
+			if (array == null || array.Length == 0)
+			{
+				return new int[0];
+			}
 
+			if (max < 0)
+			{
+				throw new ArgumentOutOfRangeException("max", "max必须大于等于0");
+			}
+
+			int[] countingArray = new int[max + 1];
+			for (int i = 0; i < array.Length; i++)
+			{
+				int value = getCompareValue(array[i]);
+				if (value < 0 || value > max)
+				{
+					throw new ArgumentOutOfRangeException("array", "数组元素必须大于等于0并小于等于max");
+				}
+
+				countingArray[getCompareValue(array[i])] += 1;
+			}
+
+			for (int i = 1; i <= max; i++)
+			{
+				countingArray[i] += countingArray[i - 1];
+			}
+
+			int[] sortedArray = new int[array.Length];
+
+			//这样是为了保持排序的稳定性
+			if (asc)
+			{
+				for (int i = array.Length - 1; i >= 0; i--)
+				{
+					int value = getCompareValue(array[i]);
+					int index = countingArray[value] - 1;
+					sortedArray[index] = array[i];
+					countingArray[value] -= 1;
+				}
+			}
+			else
+			{
+				for (int i = 0; i < array.Length; i++)
+				{
+					int value = getCompareValue(array[i]);
+					int index = array.Length - countingArray[value];
+					sortedArray[index] = array[i];
+					countingArray[value] -= 1;
+				}
+			}
+
+
+			return sortedArray;
+		}
+
+		/// <summary>
+		/// 基数排序
+		/// </summary>
+		/// <param name="array">排序的数组</param>
+		/// <param name="maxPlaces">基数排序所排的最高位</param>
+		/// <returns></returns>
+		public static int[] RadixSort(int[] array, int maxPlaces, bool asc = true)
+		{
+			if (maxPlaces <= 0)
+			{
+				throw new ArgumentOutOfRangeException("maxPlaces", "最高位必须大于0");
+			}
+
+			int baseNum = 1;
+
+			for (int i = 0; i < maxPlaces; i++)
+			{
+				array = CountingSort(array, n => (n / baseNum) % 10, 9, asc);
+				baseNum *= 10;
+			}
+
+			return array;
+		}
     }
 }
--- Sort.Test/8.LinearTimeSort/LinearTimeSortTest.cs
+++ Sort.Test/8.LinearTimeSort/LinearTimeSortTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Sort.Implements._8.LinearTimeSort;
+using Sort.Implements;
 
 namespace Sort.Test._8.LinearTimeSort
 {
@@ -32,5 +32,56 @@ namespace Sort.Test._8.LinearTimeSort
             Assert.AreEqual(5, sortedArray[3]);
             Assert.AreEqual(1, sortedArray[4]);
         }
-    }
+
+		[TestMethod]
+		public void Radix_Sort_Array_More_Than_One_Element_ASC()
+		{
+			int[] array = new int[] { 611, 833, 129, 645, 521 };
+			int[] sortedArray = LinearTimeSorter.RadixSort(array, 3);
+
+			Assert.AreEqual(129, sortedArray[0]);
+			Assert.AreEqual(521, sortedArray[1]);
+			Assert.AreEqual(611, sortedArray[2]);
+			Assert.AreEqual(645, sortedArray[3]);
+			Assert.AreEqual(833, sortedArray[4]);
+		}
+
+		[TestMethod]
+		public void Radix_Sort_Array_More_Than_One_Element_DESC()
+		{
+			int[] array = new int[] { 611, 833, 129, 645, 521 };
+			int[] sortedArray = LinearTimeSorter.RadixSort(array, 3, false);
+
+			Assert.AreEqual(129, sortedArray[4]);
+			Assert.AreEqual(521, sortedArray[3]);
+			Assert.AreEqual(611, sortedArray[2]);
+			Assert.AreEqual(645, sortedArray[1]);
+			Assert.AreEqual(833, sortedArray[0]);
+		}
+
+		[TestMethod]
+		public void Radix_Sort_Place_Less_Than_Element_Place()
+		{
+			int[] array = new int[] { 199, 931, 256, 412 };
+			int[] sortedArray = LinearTimeSorter.RadixSort(array, 2);
+
+			Assert.AreEqual(412, sortedArray[0]);
+			Assert.AreEqual(931, sortedArray[1]);
+			Assert.AreEqual(256, sortedArray[2]);
+			Assert.AreEqual(199, sortedArray[3]);
+		}
+
+		[TestMethod]
+		public void Radix_Sort_Place_More_Than_Element_Max_Place()
+		{
+			int[] array = new int[] { 611, 833, 49, 645, 81 };
+			int[] sortedArray = LinearTimeSorter.RadixSort(array, 5);
+
+			Assert.AreEqual(49, sortedArray[0]);
+			Assert.AreEqual(81, sortedArray[1]);
+			Assert.AreEqual(611, sortedArray[2]);
+			Assert.AreEqual(645, sortedArray[3]);
+			Assert.AreEqual(833, sortedArray[4]);
+		}
+	}
 }
